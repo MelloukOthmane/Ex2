4d15d5ce54b2f8cd6a0730052df9758dce45ce6c
MelloukOthmane <othmane.mellouk@usmba.ac.ma>
2022-10-31 13:23:05 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            int[] tab = new int[] { 10, 20, 30, 40, 50 , 60};
+            Console.Write(" N : ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 5; i > 0; i--)
+            {
+                if (tab[i - 1] > n)
+                {
+                    tab[i] = tab[i - 1];
+                    tab[i - 1] = n;
+                }
+            }
+            for (i = 0; i < 6; i++)
+            {
+                Console.Write("\t" + tab[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
